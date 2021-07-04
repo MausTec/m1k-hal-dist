d6c541fb99b0308ed4b3a58ceaa919446eadbbff
Mau Abata <mauabata@gmail.com>
2021-07-04 15:15:58 -0500

    DIST

--- m1k-hal-strings.hpp
+++ m1k-hal-strings.hpp
@@ -6,7 +6,7 @@
 /**
  * Automatically generated string value array for the m1k_hal_err enum.
  */
-const char *m1k_hal_err_str[] = {
+static const char *m1k_hal_err_str[] = {
     "M1K_HAL_OK",
     "M1K_HAL_FAIL",
     "M1K_HAL_ERR_HARDWARE_LOCKED",
@@ -16,21 +16,21 @@ const char *m1k_hal_err_str[] = {
 /**
  * Automatically generated string value array for the m1k_hal_hardware_type enum.
  */
-const char *m1k_hal_hardware_type_str[] = {
+static const char *m1k_hal_hardware_type_str[] = {
     "M1K_HAL_HARDWARE_TYPE_MERCURY_1000",
 };
 
 /**
  * Automatically generated string value array for the m1k_hal_button enum.
  */
-const char *m1k_hal_button_str[] = {
+static const char *m1k_hal_button_str[] = {
     "M1K_HAL_BUTTON_MENU",
 };
 
 /**
  * Automatically generated string value array for the m1k_hal_accessory_mode enum.
  */
-const char *m1k_hal_accessory_mode_str[] = {
+static const char *m1k_hal_accessory_mode_str[] = {
     "M1K_HAL_ACCESSORY_DISABLED",
     "M1K_HAL_ACCESSORY_SOURCE",
     "M1K_HAL_ACCESSORY_TARGET",
